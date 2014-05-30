644bd6f213dc8c68bae0e4a11c7b2f5796979d23
Rich Torres <rich@discovix.com>
2014-05-29 18:34:25 -0700

    adding styling, moving navbar and creating a url formatting service

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -5,12 +5,6 @@ app.controller('PostsCtrl', function($scope, Post) {
 	
   $scope.post = {url: 'http://', title: ''};
 
-  $scope.submitPost = function () {
-    Post.create($scope.post).then( function () {
-      $scope.post = {url: 'http://', title: ''};
-    });
-  };
-
   $scope.deletePost = function (postId) {
     Post.delete(postId);
   };
