b307d99ae940004ee30e14257307ce3e2aa12244
Rich Torres <rich@discovix.com>
2014-05-29 18:22:57 -0700

    stubbing our link show page. Fixing links so they actually send you to the proper page

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,21 +1,17 @@
 'use strict';
 
 app.controller('PostsCtrl', function($scope, Post) {
-  $scope.posts = [];
-	$scope.post = {url: 'http//', title: ''};
+  $scope.posts = Post.all;
+	
+  $scope.post = {url: 'http://', title: ''};
 
   $scope.submitPost = function () {
-    $scope.posts.push($scope.post);
-    Post.save($scope.post, function (ref) {
-      $scope.posts[ref.name] = $scope.post;
+    Post.create($scope.post).then( function () {
       $scope.post = {url: 'http://', title: ''};
     });
-    $scope.post = {url: 'http://', title: ''};
   };
 
   $scope.deletePost = function (postId) {
-    Post.delete({id: postId}, function () {
-      delete $scope.posts[postId];
-    });
+    Post.delete(postId);
   };
 });
