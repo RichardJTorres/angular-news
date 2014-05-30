5b7c8b88853f6b509a8a8afb2707599ce534dc7c
Rich Torres <rich@discovix.com>
2014-05-29 21:44:24 -0700

    adding ability to login

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,6 +19,10 @@ app.config(function ($routeProvider) {
       templateUrl: 'views/showpost.html',
       controller: 'PostViewCtrl'
     })
+    .when('/register', {
+      templateUrl: 'views/register.html',
+      controller: 'AuthCtrl'
+    })
     .otherwise({
       redirectTo: '/'
     });
---
+++ app/scripts/controllers/auth.js
@@ -0,0 +1,17 @@
+'use strict';
+
+app.controller('AuthCtrl', function ($scope, $location, Auth) {
+  if (Auth.signedIn()) {
+    $location.path('/');
+  }
+
+  $scope.register = function () {
+    console.log('register')
+    Auth.register($scope.user).then(function (authUser) {
+        console.log(authUser);
+        $location.path('/');
+      });
+  };
+
+
+});
