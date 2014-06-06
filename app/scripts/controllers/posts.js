ee836ec031784bfd366fd5db6f5254f84251c57a
Rich Torres <rich@discovix.com>
2014-06-05 21:37:22 -0700

    adding posts to user profile.

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,11 +1,13 @@
 'use strict';
 
-app.controller('PostsCtrl', function($scope, Post) {
-  $scope.posts = Post.all;
-	
-  $scope.post = {url: 'http://', title: ''};
+app.controller('PostsCtrl', function($scope, $location, Post) {
+    if ($location.path() === '/') {
+      $scope.posts = Post.all;
+    }
 
-  $scope.deletePost = function (postId) {
-    Post.delete(postId);
-  };
+    $scope.post = {url: 'http://', title: ''};
+
+    $scope.deletePost = function (postId) {
+        Post.delete(postId);
+     };
 });
